0d7a4bcd7fdc89da45fba120398840674c877d38
Gatadev <kairateadoro@gmail.com>
2022-06-15 00:00:14 -0500

    XD

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from"react";
+import"./appCss/app.css";
+import Clock from "./components/clock";
 
-function App() {
-  return (
+
+function App(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+<div class="contenedor">
+
+<nav>
+
+    <a class="logoName" href="#"><b>gtdev</b></a>
+    
+<div class="navegacion">
+    <a href="#">Youtube</a>
+    <a href="#">Discord</a>
+    <a href="#">GitHub</a> 
+</div>
+
+<div class="reloj">
+    <Clock></Clock>
+</div>
+
+</nav>
+
+</div>
+
+
+  
+    
+  </div>
+    
+ );
 }
 
 export default App;
